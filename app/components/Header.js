55da7cc0fa00206d5987c559eb73da8e5d07fdb9
charles peters <charlespeters42@gmail.com>
2016-10-27 19:25:02 -0700

    updating header

--- app/components/Header.js
+++ app/components/Header.js
@@ -2,7 +2,8 @@ import React from 'react'
 
 const Header = props =>
   <header className='o-container--full o-container--center u-py5 u-px2'>
-    <h1 className='h6 u-w500'>{props.name}</h1>
+    <div className='Ball' />
+    <h1 className='h6 u-w500 u-inbl'>{props.name}</h1>
   </header>
 
 Header.defaultProps = {
