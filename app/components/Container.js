0a31fa1a6e3fc30313c61a49683c5e03a63c3b5e
Charles ⚡ <charlespeters42@gmail.com>
2017-06-25 18:07:10 -0700

    updating component

--- app/components/Container.js
+++ app/components/Container.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import Card from './Card'
 
-const Container = props =>
+export default ({ shots, user }) =>
   <div className='c-card__container o-container--full o-container--center o-flex o-flex--wrap o-flex--j--c'>
-    {props.shots.map(shot => {
+    {shots.map(shot => {
       console.log(shot)
       return (
         <Card
@@ -12,7 +12,7 @@ const Container = props =>
           image={shot.images.hidpi ? shot.images.hidpi : shot.images.normal}
           url={shot.html_url}
           description={shot.description}
-          user={props.user}
+          user={user}
           likes={shot.likes_count}
           views={shot.views_count}
           rebounds={shot.rebounds_count}
@@ -20,5 +20,3 @@ const Container = props =>
       )
     })}
   </div>
-
-export default Container
