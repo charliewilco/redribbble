2a5e87e61f1672a0f102f69f84aa94556108025e
Charles ⚡ <charlespeters42@gmail.com>
2017-06-25 18:30:58 -0700

    moving to CRA

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import DOM from 'react-dom'
 import App from './components/App'
-
+import 'obsidian.css/dist/obsidian.css'
+import 'ff.css'
 const token = '52cd650e606e842d63ab9ebf8e595710a0b050eea7015514adfc99d6a0e3661a'
 
-DOM.render(<App source={token} />, document.getElementById('app'))
+DOM.render(<App source={token} />, document.getElementById('root'))
